752471b1ab01069b4d9e50bdd2ff5e422b0f8440
KyllianGenot <kygenot@gmail.com>
2025-03-21 09:47:24 +0100

    Update port

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 4000,
     mongoDbUrl: process.env.MONGODB_URL,
 };
