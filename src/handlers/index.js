fce2cdb3f6878f8090d0f118f44198be63f0c9ff
KyllianGenot <kygenot@gmail.com>
2025-03-21 11:25:11 +0100

    Add fastify CORS

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -5,7 +5,7 @@ export default function addRouteHandlers(app) {
     app.get("/", async (request, reply) => {
         return { hello: "world" };
     });
-    app.post("/signup", async (request, reply) => {
+    app.post("/api/users", async (request, reply) => {
         const { email, password, username } = request.body;
         app.log.info(`Creating user ${username}`);
         const user = new User({ 
