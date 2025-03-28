4f9263658b15922be5758b6bd1583bb874b8109d
KyllianGenot <kygenot@gmail.com>
2025-03-28 17:21:27 +0100

    chore: ⚙️ add jwt configuration

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import fastify from "fastify";
 import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
 import config from "./config.js";
 import connect from "./connect.js";
 import addRouteHandlers from "./handlers/index.js";
 
 const app = fastify({ logger: true });
-await app.register(cors,{});
+
+await app.register(cors, {});
+
+await app.register(jwt, {
+    secret: config.jwtSecret,
+});
+
 addRouteHandlers(app);
 
 try {
     await connect();
-    await app.listen({ port: config.port, host: '0.0.0.0' });} 
-catch (error) {
+    await app.listen({ port: config.port, host: "0.0.0.0" });
+} catch (error) {
     app.log.error(error);
     process.exit(1);
 }
